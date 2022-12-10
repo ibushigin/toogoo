e0f29543e1dadc13b8eb86ce49c2b270edad2903
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2022-12-10 19:19:17 +0100

    add token.json creation

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
 from tgtg import TgtgClient
 import json
+from pathlib import Path
 
-#import of your tokens
-f = open('token.json')
-tokens = json.load(f)
+#Create token
+pathToFile = 'token.json'
+path = Path(pathToFile)
+if path.is_file():
+    print("Token found")
+else:    
+    client = TgtgClient(email="jexn.lxlxnne@gmail.com")
+    credentials = client.get_credentials()
+    print(credentials)
+    with open("token.json", "w") as outfile:
+        json.dump(credentials, outfile)
+    print("Json created")
+
+#import token and get items
+f2 = open('token.json')
+tokens = json.load(f2)
 client = TgtgClient(
     access_token=tokens['access_token'],
     refresh_token=tokens['refresh_token'],
     user_id=tokens['user_id']
     )
 items = client.get_items()
+f2.close()
 
 #check if basket is available
 def ItemsAvailable():
-    for i in items:
-        if 0 == i["items_available"]:
+    for i in items:        
+        if 0 == i["items_available"]:            
             return "Il n'y a pas de panier disponible"
         else:
             list = []
             for i in items:
                 if i["items_available"] != 0:
-                    price = i["item"]["price_including_taxes"]["minor_units"]/100
+                    value = i["item"]["value_including_taxes"]["minor_units"]/100
+                    newPrice = i["item"]["price_including_taxes"]["minor_units"]/100
                     item = {
                         "shop" : i["store"]["store_name"],
-                        "itemNumber" : i["items_available"],
-                        "price" : str(price)+"€"
+                        "number of item" : i["items_available"],
+                        "value" : str(value)+"€",
+                        "new price" : str(newPrice)+"€"
                     }
                     list.append(item)    
         return list
--- getToken.py
+++
@@ -1,7 +0,0 @@
-from tgtg import TgtgClient
-# Pour récupérer les tokens
-# Ajouter l'email
-client = TgtgClient(email="email")
-credentials = client.get_credentials()
-
-print(credentials)
